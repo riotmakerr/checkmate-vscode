235e63110f256754088923156a03ebe0de7ab680
Ryan Faulhaber <faulhaber.14@osu.edu>
2018-12-12 20:09:04 -0500

    added execute anonymous stuff

---
+++ src/commands.ts
@@ -0,0 +1,26 @@
+import * as rp from 'request-promise';
+
+export function scheduleBatch(filename: string, schedule: string) {
+    console.log('filename', filename);
+    console.log('schedule', schedule);
+}
+
+export function executeBatch(fileBody: string) {
+    console.log('making request');
+
+    const opts = {
+        method: 'POST',
+        uri: 'http://localhost:56248/app/sfdx/deployclass',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: {
+            code: fileBody
+        },
+        json: true
+    };
+
+    rp.post(opts).then(() => {
+        console.log('success');
+    }).catch(err => console.log('err', err));
+}
--- src/extension.ts
+++ src/extension.ts
@@ -2,18 +2,39 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
-import * as rp from 'request-promise'; 
+import * as rp from 'request-promise';
+import * as fs from 'fs';
+import { scheduleBatch, executeBatch } from './commands';
 
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
-    const scheduleBatchCmd = vscode.commands.registerCommand('extension.scheduleBatch', () => {
+    const scheduleBatchCmd = vscode.commands.registerCommand('extension.scheduleBatch', (uri: vscode.Uri) => {
         // insert schedule batch 
+
+        vscode.window.showInputBox({ prompt: "Enter the cron schedule, or leave blank if none" })
+            .then(input => {
+                if (input) {
+                    scheduleBatch(uri.path, input);
+                }
+            });
     });
 
-    const executeBatchCmd = vscode.commands.registerCommand('extension.executeBatch', () => {
+    const executeBatchCmd = vscode.commands.registerCommand('extension.executeBatch', (uri: vscode.Uri) => {
         // insert execute batch
-    });
+
+        const fileBuf = fs.readFileSync(uri.path);
+
+        const fileStr = fileBuf.toString();
+        console.log('fileStr', fileStr);
+
+        // if (fileStr.toLowerCase().match(/System\.schedule/g)) {
+        if (fileStr !== '') {
+            executeBatch(fileStr);
+        } else {
+            vscode.window.showErrorMessage('This is not a batch script');
+        }
+    })
 
     const insertAssetLibCmd = vscode.commands.registerCommand('extension.insertFromAssetLibrary', () => {
         //var base64auth = btoa('kbhas:G4tLPa8nxhgQMxd9EHdD');
@@ -25,11 +46,13 @@ export function activate(context: vscode.ExtensionContext) {
             }
         };
 
-        rp(options) 
-            .then((response) => console.log('here? - ' + response)) 
+        rp(options)
+            .then((response) => console.log('here? - ' + response))
             .catch(err => console.error('it failed!', err));
         // curl -u kbhas:G4tLPa8nxhgQMxd9EHdD -X GET -H "Content-Type: application/json" https://api.bitbucket.org/2.0/repositories/acumensolutions/assetlibrary/src/master/apex/common/SObjectUtility.cls
     });
+
+    context.subscriptions.push(scheduleBatchCmd, executeBatchCmd, insertAssetLibCmd);
 }
 
 // this method is called when your extension is deactivated
